e5566aa9140915b4aaca3b4f84e80bfdbef2ef11
zengwenqi <2811456516@qq.com>
2024-06-19 20:37:12 +0800

    fourth commit

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello git2!");
         System.out.println("Hello git3!");
         System.out.println("Hello hot-fix!");
+        System.out.println("Hello hot-fix redis!");
     }
 }
