4efc1fa0181a7212b494c678636098fbca4ba68c
zengwenqi <2811456516@qq.com>
2024-06-19 20:31:56 +0800

    hot-fix commit

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello git!");
         System.out.println("Hello git2!");
         System.out.println("Hello git3!");
+        System.out.println("Hello hot-fix!");
     }
 }
