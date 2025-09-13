1addb451e363054cda2cf57298e391fc0c737323
zengwenqi <17784736184@163.com>
2025-09-13 17:48:35 +0800

    提交本地对Main.java的修改

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello git!");
         System.out.println("Hello git2!");
         System.out.println("Hello git3!");
+System.out.println("2025-09-13");
         System.out.println("Hello hot-fix!");
         System.out.println("Hello hot-fix redis!");
         System.out.println("Hello hot-fix wodlixi!");
